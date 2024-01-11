572dfcf9fd4cc0317b8462be3847d91fbd31d328
GeneAbril <105821014+GeneAbril@users.noreply.github.com>
2024-01-11 13:44:14 -0300

    firts app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NadvarComponent } from './components/nadvar/nadvar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NadvarComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
